4c3353e854772b6f95ec6a383711586e222e91a1
kindywu <kindywu@outlook.com>
2024-06-13 10:53:40 +0800

    add argon2

---
+++ chat_server/examples/argon2.rs
@@ -0,0 +1,49 @@
+use argon2::{
+    password_hash::{rand_core::OsRng, SaltString},
+    Argon2, PasswordHash, PasswordHasher, PasswordVerifier,
+};
+use chat_server::AppError;
+
+fn hash_password(password: &str) -> Result<String, AppError> {
+    let salt = SaltString::generate(&mut OsRng);
+
+    // Argon2 with default params (Argon2id v19)
+    let argon2 = Argon2::default();
+
+    // Hash password to PHC string ($argon2id$v=19$...)
+    let password_hash = argon2
+        .hash_password(password.as_bytes(), &salt)?
+        .to_string();
+
+    Ok(password_hash)
+}
+
+fn verify_password(password: &str, password_hash: &str) -> Result<bool, AppError> {
+    let argon2 = Argon2::default();
+    let password_hash = PasswordHash::new(password_hash)?;
+
+    // Verify password
+    let is_valid = argon2
+        .verify_password(password.as_bytes(), &password_hash)
+        .is_ok();
+
+    Ok(is_valid)
+}
+
+fn main() -> anyhow::Result<()> {
+    let password = "password";
+
+    let password_hash = hash_password(password)?;
+
+    let is_valid = verify_password(password, &password_hash)?;
+
+    assert!(is_valid);
+
+    let password = "password2";
+
+    let is_valid = verify_password(password, &password_hash)?;
+
+    assert!(!is_valid);
+
+    Ok(())
+}
