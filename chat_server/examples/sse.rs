9953dcb42cfbe7ed8eb19382df1e8284324c1ee4
kindywu <kindywu@outlook.com>
2024-06-25 17:06:39 +0800

    add sse and sse_client

---
+++ chat_server/examples/sse.rs
@@ -0,0 +1,29 @@
+use axum::{
+    response::sse::{Event, KeepAlive, Sse},
+    routing::get,
+    Router,
+};
+use futures_util::stream::{self, Stream};
+use std::{convert::Infallible, time::Duration};
+use tokio_stream::StreamExt as _;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/sse", get(sse_handler));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn sse_handler() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    // A `Stream` that repeats an event every second
+    let stream = stream::repeat_with(|| Event::default().data("hi!"))
+        .map(Ok)
+        .throttle(Duration::from_secs(1));
+
+    Sse::new(stream).keep_alive(KeepAlive::default())
+}
